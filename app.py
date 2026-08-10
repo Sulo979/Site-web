b48db43eaec4757d154e6d9a5b8102d7aaf0d5e1
Sulo <anglisam08@gmail.com>
2026-08-10 21:18:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def accueil():
-    return render_template('index.html')
+  return "HELLO, WORLD!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
 
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
